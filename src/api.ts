1907a7e0fb177aa29e9d7c704c1e2c7ef1cce71e
freight4 <305freitas@gmail.com>
2025-11-26 23:37:11 -0500

    typechart

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,22 @@
 import * as fs from "fs";
 import * as PS from "pokemon-showdown";
 const Dex = PS.Dex;
+import { TypeChart } from "pokemon-showdown/dist/data/typechart";
 import { Pokemon } from "./classes/Pokemon";
 import { Species } from "./types/Species";
+
+
+//get type charts from pokemon api 
+// const typechart = Dex.TypeDataTable
+// const typejson = JSON.stringify(typechart, null, 2);
+// fs.writeFile("src/outputs/typechartjson.json", TypeChart, (err: NodeJS.ErrnoException | null) => {
+//   if (err) {
+//     console.error("Error writing JSON file:", err);
+//   } else {
+//     console.log("JSON file has been saved.");
+//   }
+// });
+console.log(TypeChart)
 
 
 // const tiers = ['OU', 'UUBL', 'UU', 'RUBL', 'RU', 'NUBL', 'NU', 'PUBL', 'PU', 'ZUBL', 'ZU', 'NFE', 'LC']
@@ -20,7 +34,7 @@ const raw_specs = Dex.species.all().filter((obj: any) =>
 
 const rawput = JSON.stringify(raw_specs, null, 2);
 
-fs.writeFile("src/rawlist.json", rawput, (err: NodeJS.ErrnoException | null) => {
+fs.writeFile("src/outputs/rawlist.json", rawput, (err: NodeJS.ErrnoException | null) => {
   if (err) {
     console.error("Error writing JSON file:", err);
   } else {
@@ -32,7 +46,7 @@ const pokemonList = raw_specs.map((sp: any) => new Pokemon(sp));
 const specslist = JSON.stringify(pokemonList, null, 2);
 
 
-fs.writeFile("src/specslist.json", specslist, (err: NodeJS.ErrnoException | null) => {
+fs.writeFile("src/outputs/specslist.json", specslist, (err: NodeJS.ErrnoException | null) => {
   if (err) {
     console.error("Error writing JSON file:", err);
   } else {
---
+++ src/types/showdown-internal.d.ts
@@ -0,0 +1,4 @@
+declare module "pokemon-showdown/dist/data/typechart" {
+  import { TypeDataTable } from "pokemon-showdown/dist/sim/dex-data";
+  export const TypeChart: TypeDataTable;
+}
